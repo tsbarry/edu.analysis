5d18879881f730aa94a0c38275f97a93df80d120
tsbarry <thiernosbarry2012@gmail.com>
2022-11-14 21:17:19 -0500

    final version of edu.analysis

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # edu.analysis
 
-using list comprehension to filter and analyze a state data. Also analyze the state math score for 8th graders. 
+using list comprehension to filter and analyze a state data. Also analyze the state math score for 8th graders. lastly creating a new list to extract YEAR AND AVERAGE SCORE then calculating the percent change.
--- analysis.py
+++ analysis.py
@@ -14,3 +14,30 @@ print(state_data)
 
 new_data = [row for row in state_data if row["AVG_MATH_8_SCORE"] != '']
 print(new_data)
+
+#create a new list called years 
+# then using a for loop we extract "YEAR" and 'AVG_MATH_8_SCORE' 
+# then we append both the YEAR and AVG_MATH_8_SCORE to the new list
+# last we print the list years
+years = []
+for row in new_data:
+     year = row["YEAR"]
+     average_score = row["AVG_MATH_8_SCORE"]
+     years.append([year, average_score])
+print(years)
+
+# here I played around with indexing to get the years and average scores
+#print(years[0][0])
+#print(years[0][1])
+#print(years[1][0])
+#print(years[1][1])
+
+# to calculate the percent change, i create a for loop with a range 
+# this range will containt the lenght of the list years, 
+# this allows us to index through it and increment by 1
+for i in range(len(years)-1):
+ raw_change = (float(years[i][1]) - float(years[i+1][1]))
+ raw_change = raw_change / float(years[i][1])
+ raw_change = raw_change * 100
+ print(raw_change) 
+    
